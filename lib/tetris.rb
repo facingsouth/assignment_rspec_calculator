360a824bac025420a724e4e0d7342d6d489a7d57
facingsouth <xin.d.liu@gmail.com>
2015-07-02 17:32:21 -0700

    add little more on tetris

--- lib/tetris.rb
+++ lib/tetris.rb
@@ -3,18 +3,31 @@
 WHITE SQUARE' unicode (U+25A1)
   -Main tetris class
     -main loop for dropping pieces
-        -call - check full line?
-             if yes-remove line, if no - loop
+      -board.render
+      -get player input: (L)eft, (R)ight or (T)ransfer
+        - if L, shape move to left by 1
+        - elsif R, shape move to right by 1
+        - elsif T, shape transfer
+        - else do nothing
+      - shape move down by 1
+      -call - check full line?
+          if yes-remove line
+      - break if game_over?
 
   -board class
     - builds the 10 X 18 game board
+    -render board 
     - drop piece
-    -render board
-    -full line method check.
+    -full line check method
     
   -shapes class
     - builds all the game piece shapes
      -O (square)  -S     -L      -T
      -I           -Z     -J
+
+    - shape move left
+    - shape move right 
+    - shape transfer
+    - shape move down
 
 =end
